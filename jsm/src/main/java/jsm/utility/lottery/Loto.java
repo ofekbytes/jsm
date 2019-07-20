455be19720b17986ae747ad9d87d801fd5d32596
koskas <koskas@yahoo.com>
2019-07-20 20:54:11 +0300

    loto

---
+++ jsm/src/main/java/jsm/utility/lottery/Loto.java
@@ -0,0 +1,33 @@
+package jsm.utility.lottery;
+
+public class Loto implements InterfaceLoto {
+
+	public Loto() {
+		
+	}
+
+	@Override
+	public void createLotoNumbers() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int createRandomNumber(int intMinNumber, int intMaxNumber) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean checkIfNumberExists(int number) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void createLotoStrongNumber() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
